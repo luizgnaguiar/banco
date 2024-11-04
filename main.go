effae1446bac7f3fe4fea793a441127f9c8eef07
luizgnaguiar <117757696+luizgnaguiar@users.noreply.github.com>
2024-11-04 16:32:03 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/luizgnaguiar/banco/contas"
+	c "github.com/luizgnaguiar/banco/contas"
 )
 
 func main() {
-	contaDoLuiz := contas.ContaCorrente{"Luiz", 222, 11111, 200}
-	contaDaMari := contas.ContaCorrente{"Mari", 222, 11112, 1000}
+	contaDoLuiz := c.ContaCorrente{Titular: "Luiz", NumeroAgencia: 222, NumeroConta: 11111, Saldo: 200}
+	contaDaMari := c.ContaCorrente{Titular: "Mari", NumeroAgencia: 222, NumeroConta: 11112, Saldo: 1000}
 	status, valor := contaDoLuiz.Depositar(2500)
 	fmt.Println(status, valor)
 	contaDaMari.Transferir(500, &contaDoLuiz)
